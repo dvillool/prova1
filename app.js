47ea6bbf90772899c07d8ef1de75d074fa43eaf1
3lv27 <elvin.gm27@gmail.com>
2017-11-18 18:13:54 +0100

    primerr comitooo

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('git sucks');
+
+function tiaPepa () {
+    let compra;
+  for (let index = 0; index < 20; index++) {
+      compra += array[index];
+      
+  }
+}
+
+console.log('compra');
